51d2b438040bab31cccd4cc84b99e98c4ca3f54a
Bukowa <gitbukowa@gmail.com>
2025-02-23 00:48:31 +0100

    add test toggle

--- src/tests/test_toggle.lua
+++ src/tests/test_toggle.lua
@@ -5,7 +5,10 @@ require('consul')
 m_root = {
     Find = function(_, name)
         if name == CONSUL.COMPONENT.CONSOLE.ROOT then
-            return CONSULSCRIPTUM_COMPONENT
+            return CONSULSCRIPTUM_CONSOLE_COMPONENT
+        end
+        if name == CONSUL.COMPONENT.ROOT then
+            return CONSULSCRIPTUM_ROOT_COMPONENT
         end
     end
 }
@@ -25,34 +28,64 @@ end
 
 
 -- Mock UIComponent instance
-CONSULSCRIPTUM_COMPONENT = UIComponent()
-CONSULSCRIPTUM_COMPONENT:SetVisible(true)
+CONSULSCRIPTUM_CONSOLE_COMPONENT = UIComponent()
+CONSULSCRIPTUM_CONSOLE_COMPONENT:SetVisible(true)
 
 -- Import the module
 local TOGGLE = require('consul.consul_toggle')
 
--- Test function
-local function test_can_toggle_visibility()
+-- When you minimize the Scriptum listview it should also minimize the console
+local function test_can_toggle_visibility_of_console_scriptum()
 
     -- Initial visibility check
-    local visible = CONSULSCRIPTUM_COMPONENT:Visible()
+    local visible = CONSULSCRIPTUM_CONSOLE_COMPONENT:Visible()
     assert(visible == true, "Initial visibility should be true")
 
     -- Toggle visibility
     TOGGLE.OnComponentLClickUp({
         string = CONSUL.COMPONENT.CONSOLE.TOGGLE_BUTTON
     })
-    visible = CONSULSCRIPTUM_COMPONENT:Visible()
+    visible = CONSULSCRIPTUM_CONSOLE_COMPONENT:Visible()
     assert(visible == false, "Visibility should be false after toggling")
 
     -- Toggle visibility again
     TOGGLE.OnComponentLClickUp({
         string = CONSUL.COMPONENT.CONSOLE.TOGGLE_BUTTON
     })
-    visible = CONSULSCRIPTUM_COMPONENT:Visible()
+    visible = CONSULSCRIPTUM_CONSOLE_COMPONENT:Visible()
     assert(visible == true, "Visibility should be true after toggling again")
 
 end
 
-test_can_toggle_visibility()
-print("All tests passed")
+
+CONSULSCRIPTUM_ROOT_COMPONENT = UIComponent()
+CONSULSCRIPTUM_ROOT_COMPONENT:SetVisible(false)
+
+-- When you toggle the Consul Scriptum root it should work
+local function test_can_toggle_visibility_of_consul_scriptum_root()
+
+    -- Initial visibility check
+    local visible = CONSULSCRIPTUM_ROOT_COMPONENT:Visible()
+    assert(visible == false, "Initial visibility should be false")
+
+    -- Toggle visibility
+    TOGGLE.OnComponentLClickUp({
+        string = CONSUL.COMPONENT.BUTTON_TOGGLE.ROOT
+    })
+    visible = CONSULSCRIPTUM_ROOT_COMPONENT:Visible()
+    assert(visible == true, "Visibility should be true after toggling")
+
+    -- Toggle visibility again
+    TOGGLE.OnComponentLClickUp({
+        string = CONSUL.COMPONENT.BUTTON_TOGGLE.ROOT
+    })
+    visible = CONSULSCRIPTUM_ROOT_COMPONENT:Visible()
+    assert(visible == false, "Visibility should be false after toggling again")
+
+end
+
+
+test_can_toggle_visibility_of_console_scriptum()
+print("Test 1 passed!")
+test_can_toggle_visibility_of_consul_scriptum_root()
+print("Test 2 passed!")
