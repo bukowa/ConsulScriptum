dab55ca5f7176ba5076ad9da0924bb2f92ee14dc
Bukowa <gitbukowa@gmail.com>
2025-02-23 00:37:37 +0100

    add buttons toggles

--- src/consul/consul.lua
+++ src/consul/consul.lua
@@ -3,15 +3,14 @@ CONSUL = {
     AUTHOR = "Mateusz Kurowski",
     CONTACT = "gitbukowa@gmail.com",
 
-    ROOT_COMPONENT_NAME = "consul_scriptum",
-    CONSOLE_COMPONENT_NAME = "consul_scriptum_console",
-    CONSOLE_TOGGLE_BUTTON_NAME = "button_minimise_scriptum",
-
     COMPONENT = {
         ROOT = "consul_scriptum",
+        BUTTON_TOGGLE = {
+            ROOT = "consul_scriptum_button_toggle",
+        },
         CONSOLE = {
             ROOT = "consul_scriptum_console",
-            TOGGLE_BUTTON = "button_minimise_scriptum"
+            TOGGLE_BUTTON = "consul_scriptum_console_button_minimise_scriptum"
         }
     }
 }
