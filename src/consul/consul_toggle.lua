aae51e1d5d8c226cdf6ac963f22be4d73cdb7ad6
Bukowa <gitbukowa@gmail.com>
2025-02-23 01:42:06 +0100

    add move  to center func

--- src/consul/consul_toggle.lua
+++ src/consul/consul_toggle.lua
@@ -3,6 +3,26 @@ require 'consul'
 -- Create a logger for this script
 local log_consul_toggle = require 'consul_logging'.new_logger("consul_toggle")
 
+
+-- Moves the consul root to the center of the screen
+local function MoveConsulRootToCenter(component)
+
+    -- Get the screen dimensions
+    local screenWidth = m_root:Width()
+    local screenHeight = m_root:Height()
+
+    -- Element dimensions
+    local elementWidth = 700
+    local elementHeight = 500
+
+    -- Calculate the center position
+    local centerX = (screenWidth / 2) - (elementWidth / 2)
+    local centerY = (screenHeight / 2) - (elementHeight / 2)
+
+    -- Move the element to the calculated position
+    component:MoveTo(centerX+elementWidth, centerY)
+end
+
 -- Toggles the visibility of the console
 local function OnComponentLClickUp(context)
 
@@ -23,6 +43,7 @@ local function OnComponentLClickUp(context)
                 CONSUL.COMPONENT.ROOT
         ))
         log_consul_toggle:debug("Current visibility: " .. tostring(consul_root:Visible()))
+        MoveConsulRootToCenter(consul_root)
         consul_root:SetVisible(not consul_root:Visible())
         log_consul_toggle:debug("New visibility: " .. tostring(consul_root:Visible()))
         return
