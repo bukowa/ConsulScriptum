250974afbe83bb6ce66487eab23f9700ba4e7ef8
Bukowa <gitbukowa@gmail.com>
2025-09-19 17:34:05 +0200

    chore: add units and unit controller to autocomplete battle api

--- game_api/api.lua
+++ game_api/api.lua
@@ -158,6 +158,7 @@ local army = {}
 function army:new() end
 function army:army_handicap() end
 function army:change_faction() end
+---@return battle.unit_controller
 function army:create_unit_controller() end
 function army:enable_army_destruction_morale_effect() end
 function army:get_reinforcement_ships() end
@@ -166,7 +167,139 @@ function army:is_commander_alive() end
 function army:is_commander_invincible() end
 function army:quit_battle() end
 function army:ships() end
+---@return battle.units
 function army:units() end
+
+---@class battle.units
+local units = {}
+
+function units:new() end
+function units:count() end
+function units:kill_commander() end
+function units:mountable_artillery_item() end
+
+---@return battle.unit
+function units:item(index) end
+
+---@class battle.unit
+local unit = {}
+
+function unit:new() end
+function unit:ammo_left() end
+function unit:bearing() end
+function unit:can_perform_special_ability() end
+function unit:current_special_ability() end
+function unit:deploy_reinforcement() end
+function unit:has_ships() end
+function unit:initial_number_of_men() end
+function unit:is_artillery() end
+function unit:is_behaviour_active() end
+function unit:is_cavalry() end
+function unit:is_currently_garrisoned() end
+function unit:is_dismounted_ships() end
+function unit:is_hidden() end
+function unit:is_idle() end
+function unit:is_infantry() end
+function unit:is_leaving_battle() end
+function unit:is_limbered_artillery() end
+function unit:is_moving() end
+function unit:is_moving_fast() end
+function unit:is_routing() end
+function unit:is_shattered() end
+function unit:is_under_missile_attack() end
+function unit:is_valid_target() end
+function unit:is_visible_to_alliance() end
+function unit:kill_number_of_men() end
+function unit:missile_range() end
+function unit:name() end
+function unit:number_of_enemies_killed() end
+function unit:number_of_men_alive() end
+function unit:ordered_position() end
+function unit:ordered_width() end
+function unit:play_anim_for_captain() end
+function unit:position() end
+function unit:position_of_officer() end
+function unit:set_current_ammo_unary() end
+function unit:starting_ammo() end
+function unit:trigger_sound_charge() end
+function unit:trigger_sound_taunt() end
+function unit:trigger_sound_warcry() end
+function unit:type() end
+function unit:unary_of_men_alive() end
+function unit:unit_distance() end
+function unit:unit_in_range() end
+
+---@class battle.unit_controller
+local unit_controller = {}
+
+function unit_controller:new() end
+function unit_controller:add_all_units() end
+function unit_controller:add_group() end
+function unit_controller:add_units() end
+function unit_controller:attack_building() end
+function unit_controller:attack_building_q() end
+function unit_controller:attack_line() end
+function unit_controller:attack_line_q() end
+function unit_controller:attack_location() end
+function unit_controller:attack_location_q() end
+function unit_controller:attack_unit() end
+function unit_controller:attack_unit_q() end
+function unit_controller:change_behaviour_active() end
+function unit_controller:change_current_walk_speed() end
+function unit_controller:change_enabled() end
+function unit_controller:change_fatigue_amount() end
+function unit_controller:change_group_formation() end
+function unit_controller:change_group_formation_q() end
+function unit_controller:change_move_speed() end
+function unit_controller:change_shot_type() end
+function unit_controller:change_shot_type_q() end
+function unit_controller:clear_all() end
+function unit_controller:climb_building() end
+function unit_controller:climb_building_q() end
+function unit_controller:decrement_formation_width() end
+function unit_controller:defend_building() end
+function unit_controller:defend_building_q() end
+function unit_controller:fire_at_will() end
+function unit_controller:goto_location() end
+function unit_controller:goto_location_angle_width() end
+function unit_controller:goto_location_angle_width_q() end
+function unit_controller:goto_location_q() end
+function unit_controller:halt() end
+function unit_controller:hide_unit_card() end
+function unit_controller:highlight() end
+function unit_controller:increment_formation_width() end
+function unit_controller:interact_with_deployable() end
+function unit_controller:interact_with_deployable_q() end
+function unit_controller:kill() end
+function unit_controller:leave_building() end
+function unit_controller:melee() end
+function unit_controller:morale_behavior_default() end
+function unit_controller:morale_behavior_fearless() end
+function unit_controller:morale_behavior_rout() end
+function unit_controller:occupy_vehicle() end
+function unit_controller:occupy_vehicle_q() end
+function unit_controller:occupy_zone() end
+function unit_controller:occupy_zone_q() end
+function unit_controller:perform_special_ability() end
+function unit_controller:perform_special_ability_q() end
+function unit_controller:release_control() end
+function unit_controller:rotate() end
+function unit_controller:rotate_q() end
+function unit_controller:select_deployable_object() end
+function unit_controller:set_always_visible_to_all() end
+function unit_controller:set_invincible() end
+function unit_controller:set_invisible_to_all() end
+function unit_controller:start_celebrating() end
+function unit_controller:start_taunting() end
+function unit_controller:step_backward() end
+function unit_controller:step_forward() end
+function unit_controller:take_control() end
+function unit_controller:teleport_to_location() end
+function unit_controller:trigger_sound_vo() end
+function unit_controller:update_card_existance_on_HUD() end
+function unit_controller:withdraw() end
+function unit_controller:withdraw_q() end
+
 
 ---------------------------------
 -- GLOBAL VARIABLE DEFINITIONS --
