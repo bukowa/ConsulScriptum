0995e88cc063f6050298c3934cb61d1bfeff0d1c
Bukowa <gitbukowa@gmail.com>
2025-10-03 18:38:56 +0200

    chore: bump version to 0.4.0

--- src/consul/consul.lua
+++ src/consul/consul.lua
@@ -1,6 +1,6 @@
 consul = {
 
-    VERSION = "0.3.0",
+    VERSION = "0.4.0",
     URL = "http://github.com/bukowa/ConsulScriptum",
     AUTHOR = "Mateusz Kurowski",
     CONTACT = "gitbukowa@gmail.com",
