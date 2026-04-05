b2e2ed5e8daf8ed7f66326903b9407db9395e709
Bukowa <gitbukowa@gmail.com>
2026-04-05 06:16:52 +0200

    chore: bump version to 0.6.2

--- src/consul/consul_changelog.lua
+++ src/consul/consul_changelog.lua
@@ -1,6 +1,9 @@
 local changelog = {
     header = "--------------------------------------------------------------------------------\n>> To mark as read and hide, type: /changelog_read\n>> To read again, type: /changelog\n--------------------------------------------------------------------------------\n\n",
     notes = {
+        ["0.6.2"] = {
+            common = "Fixed: Correctly load changelog module in campaign mode by fixing require path."
+        },
         ["0.6.1"] = {
             common = "Added: Changelog feature.\nAdded: Command /changelog - displays version notes.\nAdded: Command /changelog_read - marks current version as read and hides it on startup."
         },
