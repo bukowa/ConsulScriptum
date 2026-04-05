3137b51e66ba4547f1b053f3829b493c339924d5
Bukowa <gitbukowa@gmail.com>
2026-04-05 22:02:28 +0200

    chore: update changelog

--- src/consul/consul_changelog.lua
+++ src/consul/consul_changelog.lua
@@ -1,6 +1,10 @@
 local changelog = {
     header = "--------------------------------------------------------------------------------\n>> To mark as read and hide, type: /changelog_read\n>> To read again, type: /changelog\n--------------------------------------------------------------------------------\n\n",
     notes = {
+        ["0.7.0"] = {
+            common = "Added: Support for loading custom commands directly from the game directory or mods.\nAdded: Example template with instructions is available in the mod pack as `consul/consul_commands.lua`.\nAdded: Command `/reload_custom_commands` to refresh installed custom commands without restarting the game.",
+            Rome2  = "Added: Specific Divide et Impera (DEI) commands for population management (`/dei_reset_all_pop`, `/dei_set_pop`, `/dei_reset_region_pop`)."
+        },
         ["0.6.2"] = {
             common = "Fixed: Correctly load changelog module in campaign mode by fixing require path."
         },
