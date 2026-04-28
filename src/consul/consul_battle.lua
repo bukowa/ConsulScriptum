0903d9e0398ca6c65861b16e2254964861dccd54
Bukowa <gitbukowa@gmail.com>
2026-04-28 02:17:17 +0200

    fix(uidebug): run commands in battle

--- src/consul/consul_battle.lua
+++ src/consul/consul_battle.lua
@@ -1,22 +1,22 @@
 -- load in battle script library
-require "lua_scripts.Battle_Script_Header";
+require("lua_scripts.Battle_Script_Header")
 
 -- declare battlemanager object
-bm = battle_manager:new(empire_battle:new());
+bm = battle_manager:new(empire_battle:new())
 
 -- consul
-require 'consul'
+require("consul")
 
 -- ui handling
 -- UIComponent is nil when UICreated fires,
 -- we have to grab it here and recreate the UIRoot
 consul.ui._UIComponent = UIComponent
 if consul_build == "Rome2" then
-    local layout = UIComponent(bm:ui_component('layout'))
-    consul.ui._UIRoot = UIComponent(layout:Parent())
+	local layout = UIComponent(bm:ui_component("layout"))
+	consul.ui._UIRoot = UIComponent(layout:Parent())
 end
 if consul_build == "Attila" then
-    consul.ui._UIRoot = UIComponent(consul.ui._UIContextComponent)
+	consul.ui._UIRoot = UIComponent(consul.ui._UIContextComponent)
 end
 
 _G.consul = consul
@@ -26,7 +26,15 @@ consul.is_in_battle_script = true
 -- consul end
 
 if consul_build == "Attila" then
-    table.insert(events.PanelOpenedBattle, function(context)
-        consul.ui.attila.OnUICreated({})
-    end)
+	table.insert(events.PanelOpenedBattle, function(context)
+		consul.ui.attila.OnUICreated({})
+	end)
+
+	if consul.uidebug.is_hooked then
+		function __consul_attila_uidebug_battle_single_shot_timer()
+			consul.uidebug.process_commands()
+		end
+
+		consul.bm:register_repeating_timer("__consul_attila_uidebug_battle_single_shot_timer", 0.5)
+	end
 end
