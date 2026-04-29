f0660fc6ca122ff6214d8afe6f43b128b2b1496f
Bukowa <gitbukowa@gmail.com>
2026-04-29 03:47:32 +0200

    chore(release): bump version

--- src/consul/consul.lua
+++ src/consul/consul.lua
@@ -4,7 +4,7 @@ consul_build = "Attila" -- or "Rome2"
 
 consul = {
 
-	VERSION = "0.8.0",
+	VERSION = "0.8.1",
 	URL = "http://github.com/bukowa/ConsulScriptum",
 	AUTHOR = "Mateusz Kurowski",
 	CONTACT = "gitbukowa@gmail.com",
